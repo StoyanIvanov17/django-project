a172e3987b1e96830e7a5dfbd7bbcbec64f8877a
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2025-05-08 03:32:29 +0300

    navbar changes

--- staticfiles/js/navbar.js
+++ staticfiles/js/navbar.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navbar = document.querySelector(".navbar");
-    const hero = document.querySelector(".hero-video-section");
-
-    if (!navbar) return;
-
-    if (hero) {
-        const heroHeight = hero.offsetHeight;
-
-        window.addEventListener("scroll", function() {
-            if (window.scrollY > heroHeight - 100) {
-                navbar.classList.add("scrolled");
-            } else {
-                navbar.classList.remove("scrolled");
-            }
-        });
-    }
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     const navbar = document.querySelector(".navbar");
+//     const hero = document.querySelector(".hero-video-section");
+//
+//     if (!navbar) return;
+//
+//     if (hero) {
+//         const heroHeight = hero.offsetHeight;
+//
+//         window.addEventListener("scroll", function() {
+//             if (window.scrollY > heroHeight - 100) {
+//                 navbar.classList.add("scrolled");
+//             } else {
+//                 navbar.classList.remove("scrolled");
+//             }
+//         });
+//     }
+// });
