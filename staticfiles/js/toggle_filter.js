248904e27b42887632160517024c05e757316c83
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2025-05-14 22:49:52 +0300

    products page filter design

---
+++ staticfiles/js/toggle_filter.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productsGalleryWrapper = document.querySelector(".products-gallery-wrapper");
+    const toggleBtn = document.getElementById("toggle-filter");
+
+    toggleBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        productsGalleryWrapper.classList.toggle("show-filters");
+        toggleBtn.innerHTML = productsGalleryWrapper.classList.contains("show-filters")
+            ? 'Hide Filters <i class="fa-solid fa-filter"></i>'
+            : 'Show Filters <i class="fa-solid fa-filter"></i>';
+    });
+});
