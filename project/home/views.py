4972aef6ef94e7248d18616798b05324fc2d4b40
StoyanIvanov17 <stoyanivanovmail@gmail.com>
2026-06-10 22:14:56 +0300

    work on homepage

--- project/home/views.py
+++ project/home/views.py
@@ -39,4 +39,42 @@ class HomeView(views.TemplateView):
             self.request.session.pop('login_non_field_errors', None)
         )
 
+        context['sections'] = [
+            {
+                'title': 'Tennis Collection',
+                'description': 'Lightweight, flexible pieces built for the court.',
+                'button_text': 'Shop Tennis',
+                'button_url': '/products/?category=tennis',
+                'image': 'images/tennis_homepage.jpg',
+            },
+            {
+                'title': 'Ski Collection',
+                'description': 'Lightweight, flexible pieces built for the snow course.',
+                'button_text': 'Shop Ski',
+                'button_url': '/products/?category=ski',
+                'image': 'images/ski_homepage.jpg',
+            },
+            {
+                'title': 'Golf Collection',
+                'description': 'Performance apparel designed for every round.',
+                'button_text': 'Shop Golf',
+                'button_url': '/products/?category=golf',
+                'image': 'images/golf_homepage_2.jpg',
+            },
+            {
+                'title': 'Everyday Collection',
+                'description': 'Versatile styles for life beyond sport.',
+                'button_text': 'Shop Everyday',
+                'button_url': '/products/?category=everyday',
+                'image': 'images/everyday_homepage.jpg',
+            },
+            {
+                'title': 'Travel Collection',
+                'description': 'Versatile styles for life beyond sport.',
+                'button_text': 'Shop Everyday',
+                'button_url': '/products/?category=travel',
+                'image': 'images/travel_homepage.jpg',
+            },
+        ]
+
         return context
